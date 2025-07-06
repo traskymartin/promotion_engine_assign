85e33d1a2d128eb8c536fd8bd7c351fec8baaf75
traskymartin <amalatrasky15@gmail.com>
2025-07-06 13:15:57 +0530

    Show output in deploy link

--- src/main/java/org/trasky/Main.java
+++ src/main/java/org/trasky/Main.java
@@ -1,5 +1,7 @@
 package org.trasky;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,9 +21,15 @@ public class Main {
                 new CartItem('B', 5), new CartItem('C', 1),new CartItem('D',1));
 
         PromotionEngine engine = new PromotionEngine();
-        System.out.println(engine.applyPromotions(senrioA));
-        System.out.println(engine.applyPromotions(senrioB));
-        System.out.println(engine.applyPromotions(senrioC));
+         try (FileWriter writer = new FileWriter("output.txt")) {
+            writer.write("Promotion Engine Test Results\n");
+            writer.write("==========================\n\n");
+            writer.write("Scenario A (1A, 1B, 1C): " + engine.applyPromotions(senrioA) + "\n");
+            writer.write("Scenario B (5A, 5B, 1C): " + engine.applyPromotions(senrioB) + "\n");
+            writer.write("Scenario C (3A, 5B, 1C, 1D): " + engine.applyPromotions(senrioB) + "\n");
+        } catch (IOException e) {
+            System.err.println("Error writing output: " + e.getMessage());
+        }
         
     }
 }
